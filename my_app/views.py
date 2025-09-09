c6dde93e7f547afc6a93b0090f6ab4b3f0c62c33
alicia.brandewie@gmail.com <alicia@Alicia-PC.localdomain>
2025-09-09 12:33:04 -0500

    hooked up DB & horse model & admin site

--- my_app/views.py
+++ my_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-class Horse:
-    def __init__(self, name, breed, description, age, nickname):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-        self.nickname = nickname
-
-horses = [
-    Horse('Chad', 'Quarter Horse', 'Western Pleasure speed, Jumper jump', 6, 'Mojo Dojo Casa Horse'),
-    Horse('Raven', 'Thoroughbred', 'Terrifying', 8, 'Rave-gravy'),
-]
-
+from .models import Horse
 
 # Create your views here.
 
@@ -23,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def horse_index(request):
+    horses = Horse.objects.all()
     return render(request, 'horses/index.html', {'horses': horses})
